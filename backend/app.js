f55693881edbf619441b99c42446235c6001c94f
Amirsohail335 <amirsohail335@gmail.com>
2024-03-08 17:03:38 +0530

    added AppPro for styling

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,25 @@
 // const express = require("express");
 // const app = express();
+// const morgan = require("morgan");
+// const mongoose = require("mongoose");
 
 // require("dotenv/config");
+
+// // //middleware
+// app.use(express.json());
+// app.use(morgan("tiny"));
 
 // const api = process.env.API_URL;
 
 // //http://localhost:3001/api/v1/products
+
+// const productSchema = mongoose.Schema({
+//   name: String,
+//   image: String,
+//   countInStock: Number,
+// });
+
+// const Product = mongoose.model("Product", productSchema);
 
 // app.get(`${api}/products`, (req, res) => {
 //   const product = {
@@ -15,6 +29,39 @@
 //   };
 //   res.send(product);
 // });
+
+// app.post(`${api}/products`, (req, res) => {
+//   const product = new Product({
+//     name: req.body.name,
+//     image: req.body.img,
+//     countInStock: req.body.countInStock,
+//   });
+//   product
+//     .save()
+//     .then((createdProduct) => {
+//       res.status(201).json(createdProduct);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({
+//         error: err,
+//         success: false,
+//       });
+//     });
+// });
+
+// //Database
+// mongoose
+//   .connect(process.env.CONNECTION_STRING, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: "eshop-database",
+//   })
+//   .then(() => {
+//     console.log("Database Connection is ready...");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // app.listen(3000, () => {
 //   console.log(api);
