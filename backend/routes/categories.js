0e3cf07e0afbf67690f03a390e5acc7d1e3541a4
Amirsohail335 <amirsohail335@gmail.com>
2024-03-29 16:53:31 +0530

    committed

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -11,14 +11,14 @@ router.get(`/`, async (req, res) =>{
     res.status(200).send(categoryList);
 })
 
-// router.get('/:id', async(req,res)=>{
-//     const category = await Category.findById(req.params.id);
+router.get('/:id', async(req,res)=>{
+    const category = await Category.findById(req.params.id);
 
-//     if(!category) {
-//         res.status(500).json({message: 'The category with the given ID was not found.'})
-//     } 
-//     res.status(200).send(category);
-// })
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found.'})
+    } 
+    res.status(200).send(category);
+})
 
 
 
@@ -37,22 +37,22 @@ router.post('/', async (req,res)=>{
 })
 
 
-// router.put('/:id',async (req, res)=> {
-//     const category = await Category.findByIdAndUpdate(
-//         req.params.id,
-//         {
-//             name: req.body.name,
-//             icon: req.body.icon || category.icon,
-//             color: req.body.color,
-//         },
-//         { new: true}
-//     )
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon || category.icon,
+            color: req.body.color,
+        },
+        { new: true}
+    )
 
-//     if(!category)
-//     return res.status(400).send('the category cannot be created!')
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
 
-//     res.send(category);
-// })
+    res.send(category);
+})
 
 router.delete('/:id', (req, res)=>{
     Category.findByIdAndRemove(req.params.id).then(category =>{
