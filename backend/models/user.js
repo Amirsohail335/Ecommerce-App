a1348db94689e1b6d66834dffeb6e971380e3306
Amirsohail335 <amirsohail335@gmail.com>
2024-04-22 13:47:37 +0530

    committed

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = mongoose.Schema({
-//     name: String,
-//     image: String,
-//     countInStock: {
-//         type: Number,
-//         required: true
-//     }
-// })
-
-// exports.User = mongoose.model('User', userSchema);
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
